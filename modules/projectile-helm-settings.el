cca4182c774a0e20cf0aec2d22b406d96638802e
jmoon <mkj71790@gmail.com>
2018-07-10 01:53:29 -0400

    small additional changes to projectile and helm

--- modules/projectile-helm-settings.el
+++ modules/projectile-helm-settings.el
@@ -1,6 +1,11 @@
 ;; projectile and helm settings here
-(projectile-global-mode)
+
+;; hook projectile mode to programming modes
+(add-hook 'prog-mode-hook 'projectile-mode)
+
+;; use helm for projectile completion
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
 
+;; use helm buffer list because it's better
 (global-set-key (kbd "C-x b") 'helm-buffers-list)

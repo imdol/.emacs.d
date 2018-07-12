b631e0b0b1e18cf61f3a3b4e1058e95a92522ad1
jmoon <mkj71790@gmail.com>
2018-07-12 06:59:54 -0400

    changed some space/tab errors for CLang

--- modules/clang-settings.el
+++ modules/clang-settings.el
@@ -1,5 +1,9 @@
 ;;CLang settings
 
+(setq-default c-basic-offset 4
+			  tab-width 4
+			  indent-tabs-mode t)
+
 ;; Load with `irony-mode` as a grouped backend
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
--- modules/typescript-settings.el
+++ modules/typescript-settings.el
@@ -1,2 +1,3 @@
 ;; If use bundled typescript.el,
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+

0f69b9e99a47fed9dd6ceb34b6fd72662c4655d1
S L <mkj71790@gmail.com>
2018-10-16 04:02:09 -0400

    Cleaned un-used configs

--- modules/exec-path-settings.el
+++
@@ -1,4 +0,0 @@
-;; only need exec-path-from-shell on OSX
-;; this hopefully sets up path and other vars better
-;(when (memq window-system '(mac ns))
- ; (exec-path-from-shell-initialize))
--- modules/smart-tabs-settings.el
+++
@@ -1,2 +0,0 @@
-;; smart-tabs config here
-; keeping this empty until I get annoyed and need it

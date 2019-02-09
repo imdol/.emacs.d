a5633eb229f9ab6abba6ef8c8603302ad6b64b74
soren <mkj71790@gmail.com>
2019-02-09 00:05:51 -0500

    small optimizations and some upgrades. runs at avg of 740ms on startup

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,8 +1,8 @@
-(add-hook 'after-init-hook 'global-company-mode)
 (company-quickhelp-mode)
 
 ;; company mappings
 (with-eval-after-load 'company
+  (setq company-idle-delay 0.25)
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
   (define-key company-active-map (kbd ".") 'company--my-insert-dot)

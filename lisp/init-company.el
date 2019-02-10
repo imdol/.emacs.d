73e3c9bbc535ab64465045008466321925f4821b
soren <mkj71790@gmail.com>
2019-02-10 01:45:15 -0500

    "changed company tooltip to inherit theme"

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -10,4 +10,13 @@
   (define-key company-active-map (kbd "C-n") #'company-select-next)
   (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
+(require 'color)
+  (let ((bg (face-attribute 'default :background)))
+    (custom-set-faces
+     `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 10)))))
+     `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+     `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+     `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+     `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
+
 (provide 'init-company)

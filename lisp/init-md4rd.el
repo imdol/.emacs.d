57551e5bb1acb5c5c86c7b59d087fa6f6f13e694
JM <mkj71790@gmail.com>
2019-06-04 00:30:37 -0400

    Small additional

--- lisp/init-md4rd.el
+++ lisp/init-md4rd.el
@@ -1,4 +1,4 @@
-(setq md4rd-subs-active '(c_programming cscareerquestions programming cpp emacs prolog))
+(setq md4rd-subs-active '(programmingcirclejerk c_programming cscareerquestions programming cpp emacs prolog))
 (defalias 'reddit 'md4rd)
 
 (provide 'init-md4rd)

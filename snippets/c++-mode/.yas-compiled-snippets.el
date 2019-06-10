651453dae2b4f8b9c1da6de42c9e14c9efd59b60
JM <mkj71790@gmail.com>
2019-06-09 23:52:07 -0400

    Added some c snippets for cast, malloc, and calloc

--- snippets/c++-mode/.yas-compiled-snippets.el
+++ snippets/c++-mode/.yas-compiled-snippets.el
@@ -2,9 +2,9 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'c++-mode
-		     '(("pra" "#pragma $0" "pragma" nil nil nil "/home/slispe/.emacs.d/snippets/c++-mode/pragma" nil nil)
-		       ("fp" "${1} ${2}(${3});$0" "function-prototype" nil nil nil "/home/slispe/.emacs.d/snippets/c++-mode/function-prototype" nil nil)
-		       ("d" "#define $0" "define" nil nil nil "/home/slispe/.emacs.d/snippets/c++-mode/define" nil nil)))
+					 '(("pra" "#pragma $0" "pragma" nil nil nil "/home/slispe/.emacs.d/snippets/c++-mode/pragma" nil nil)
+					   ("fp" "${1} ${2}(${3});$0" "function-prototype" nil nil nil "/home/slispe/.emacs.d/snippets/c++-mode/function-prototype" nil nil)
+					   ("d" "#define $0" "define" nil nil nil "/home/slispe/.emacs.d/snippets/c++-mode/define" nil nil)))
 
 
-;;; Do not edit! File generated at Sun Apr 14 17:10:33 2019
+;;; Do not edit! File generated at Sun Jun  9 23:50:32 2019
--- snippets/c-mode/.yas-compiled-snippets.el
+++ snippets/c-mode/.yas-compiled-snippets.el
@@ -2,10 +2,13 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'c-mode
-		     '(("scanf" "scanf(\"${1:format string}\"${2: ,a0,a1});\n" "scanf" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/scanf" nil nil)
-		       ("pra" "#pragma $0" "pragma" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/pragma" nil nil)
-		       ("fp" "${1} ${2}(${3});$0" "function-prototype" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/function-prototype" nil nil)
-		       ("f" "${1} ${2}(${3})\n{\n    $0\n}" "function" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/function" nil nil)))
+					 '(("scanf" "scanf(\"${1:format string}\"${2: ,a0,a1});\n" "scanf" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/scanf" nil nil)
+					   ("pra" "#pragma $0" "pragma" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/pragma" nil nil)
+					   ("ma" "malloc(sizeof($1)${2: * ${3:size}});\n$0" "malloc" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/malloc-short" nil nil)
+					   ("fp" "${1} ${2}(${3});$0" "function-prototype" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/function-prototype" nil nil)
+					   ("f" "${1} ${2}(${3})\n{\n    $0\n}" "function" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/function" nil nil)
+					   ("cas" "(${1:type} *) $0" "cast" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/cast" nil nil)
+					   ("cal" "calloc(sizeof($1)${2:, ${3:size}});\n$0" "calloc" nil nil nil "/home/slispe/.emacs.d/snippets/c-mode/calloc" nil nil)))
 
 
-;;; Do not edit! File generated at Sun Apr 14 17:10:33 2019
+;;; Do not edit! File generated at Sun Jun  9 23:50:32 2019

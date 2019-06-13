e7581f7fe0be113d8fe8207f69b468de02e5e1e2
JM <mkj71790@gmail.com>
2019-06-13 00:48:57 -0400

    Added more custom settings for spotify

--- lisp/init-spotify.el
+++ lisp/init-spotify.el
@@ -4,7 +4,8 @@
 (setq spotify-oauth2-client-id "a11d5be9d5434021b68e8ea890bcbd33")
 (setq spotify-oauth2-client-secret "aac0053e59c1452d9e107676181f5f69")
 (setq spotify-api-search-limit 30)
-(setq spotify-mode-line-refresh-interval 0)
+(setq spotify-mode-line-refresh-interval 1)
+(setq spotify-mode-line-truncate-length 15)
 (defalias 'spotify 'global-spotify-remote-mode)
 
 (provide 'init-spotify)

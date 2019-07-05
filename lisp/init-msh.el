4ccec5893a61a31dc33cd25346a975b706b97294
JM <mkj71790@gmail.com>
2019-07-05 01:13:58 -0400

    added more support for multi-term

--- lisp/init-msh.el
+++ lisp/init-msh.el
@@ -10,6 +10,7 @@
   (add-to-list 'term-bind-key-alist '("C-c C-k" . term-char-mode))
   (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
   (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
+  (add-to-list 'term-bind-key-alist '("M-\\" . multi-term))
   )
 
 (add-hook 'term-mode-hook 'setup-msh-mode)

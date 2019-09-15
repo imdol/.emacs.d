89bf9f325c288abd5df92267c9188203ce3af17e
imdol <mkj71790@gmail.com>
2019-09-15 03:30:00 -0400

    namespace change, added some support for babel evals

---
+++ lisp/init-org-babel.el
@@ -0,0 +1,27 @@
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(
+   (python . t)
+ ))
+(defun org-babel-mode-setup ()
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((C . t)
+     (python . t)
+     (emacs-lisp . t)
+     (perl . t)
+     (js . t)
+     ))
+  (require 'org-tempo)
+  (smartparens-mode +1)
+  (yas-minor-mode)
+  (org-bullets-mode +1)
+  
+  (company-mode +1)
+  (setq company-minimum-prefix-length 1)
+  (add-hook 'completion-at-point-functions 'pcomplete-completions-at-point nil t)
+  )
+
+(add-hook 'org-mode-hook 'org-babel-mode-setup)
+
+(provide 'init-org-babel)

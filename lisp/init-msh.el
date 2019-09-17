b173215e73f0ca54341c76d0b945fd9dbd6304e3
imdol <mkj71790@gmail.com>
2019-09-16 22:55:35 -0400

    set max size to reduce buffer size

--- lisp/init-msh.el
+++ lisp/init-msh.el
@@ -6,8 +6,10 @@
 ;; (setq multi-term-program "/bin/zsh")
 
 (defun setup-msh-mode ()
+  (setq term-buffer-maximum-size 500)
   (add-to-list 'term-bind-key-alist '("C-c C-j" . term-line-mode))
   (add-to-list 'term-bind-key-alist '("C-c C-k" . term-char-mode))
+
   ;; (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
   ;; (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
   (add-to-list 'term-bind-key-alist '("M-\\" . multi-term))

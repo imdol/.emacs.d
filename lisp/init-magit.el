3e30c78896c42a48ca6a55ab1667df96a7ed1f52
imdol <imdol@protonmail.com>
2019-12-29 04:12:20 -0500

    fixes

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,6 +1,6 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; set key for magit popup
-(sadmkasd-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 (provide 'init-magit)

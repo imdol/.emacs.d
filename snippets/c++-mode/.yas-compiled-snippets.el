445545b7cdcd00900f585bf61e5df0c5d7b4989b
jmoon <jmoon@openroadmedia.com>
2020-02-11 22:33:25 -0500

    add compiled snippets

--- snippets/c++-mode/.yas-compiled-snippets.el
+++ snippets/c++-mode/.yas-compiled-snippets.el
@@ -2,9 +2,9 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'c++-mode
-		     '(("pra" "#pragma $0" "pragma" nil nil nil "/home/slispe/.emacs.d/snippets/c++-mode/pragma" nil nil)
-		       ("fp" "${1} ${2}(${3});$0" "function-prototype" nil nil nil "/home/slispe/.emacs.d/snippets/c++-mode/function-prototype" nil nil)
-		       ("d" "#define $0" "define" nil nil nil "/home/slispe/.emacs.d/snippets/c++-mode/define" nil nil)))
+		     '(("pra" "#pragma $0" "pragma" nil nil nil "/home/imdol/.emacs.d/snippets/c++-mode/pragma" nil nil)
+		       ("fp" "${1} ${2}(${3});$0" "function-prototype" nil nil nil "/home/imdol/.emacs.d/snippets/c++-mode/function-prototype" nil nil)
+		       ("d" "#define $0" "define" nil nil nil "/home/imdol/.emacs.d/snippets/c++-mode/define" nil nil)))
 
 
-;;; Do not edit! File generated at Sun Nov 10 19:48:03 2019
+;;; Do not edit! File generated at Mon Jan  6 02:34:09 2020
--- snippets/web-mode/.yas-compiled-snippets.el
+++ snippets/web-mode/.yas-compiled-snippets.el
@@ -2,11 +2,11 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'web-mode
-		     '(("spancl" "<span class=\"$1\"$2>$3</span>\n" "span-class" nil nil nil "/home/slispe/.emacs.d/snippets/web-mode/spancl" nil nil)
-		       ("span" "<span>$1</span>\n" "span" nil nil nil "/home/slispe/.emacs.d/snippets/web-mode/span" nil nil)
-		       ("htmlstart" "<!DOCTYPE HTML>\n<html>\n<head>\n	<meta charset = \"UTF-8\">\n	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\">\n	<title>${1:titlename}</title>\n	<link rel=\"stylesheet\" type=\"text/css\" href=\"$2\">\n</head>\n	<body>\n	$4\n	</body>\n\n	<footer>\n	</footer>\n	<script src=\"$3\"></script>\n</html>\n" "html-start" nil nil nil "/home/slispe/.emacs.d/snippets/web-mode/htmlstart" nil nil)
-		       ("divcl" "<div class=\"$1\"$2>$3</div>" "div-class" nil nil nil "/home/slispe/.emacs.d/snippets/web-mode/divcl" nil nil)
-		       ("div" "<div>$1</div>" "div" nil nil nil "/home/slispe/.emacs.d/snippets/web-mode/div" nil nil)))
+		     '(("spancl" "<span class=\"$1\"$2>$3</span>\n" "span-class" nil nil nil "/home/imdol/.emacs.d/snippets/web-mode/spancl" nil nil)
+		       ("span" "<span>$1</span>\n" "span" nil nil nil "/home/imdol/.emacs.d/snippets/web-mode/span" nil nil)
+		       ("htmlstart" "<!DOCTYPE HTML>\n<html>\n<head>\n	<meta charset = \"UTF-8\">\n	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\">\n	<title>${1:titlename}</title>\n	<link rel=\"stylesheet\" type=\"text/css\" href=\"$2\">\n</head>\n	<body>\n	$4\n	</body>\n\n	<footer>\n	</footer>\n	<script src=\"$3\"></script>\n</html>\n" "html-start" nil nil nil "/home/imdol/.emacs.d/snippets/web-mode/htmlstart" nil nil)
+		       ("divcl" "<div class=\"$1\"$2>$3</div>" "div-class" nil nil nil "/home/imdol/.emacs.d/snippets/web-mode/divcl" nil nil)
+		       ("div" "<div>$1</div>" "div" nil nil nil "/home/imdol/.emacs.d/snippets/web-mode/div" nil nil)))
 
 
-;;; Do not edit! File generated at Sun Nov 10 19:48:03 2019
+;;; Do not edit! File generated at Mon Jan  6 02:34:09 2020

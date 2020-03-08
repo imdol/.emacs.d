126c2c90b3a72864a4efeb3732816afbc7fdd456
imdol <mkj71790@gmail.com>
2020-03-08 14:37:39 -0400

    add multi-term prev and next keys

--- lisp/init-msh.el
+++ lisp/init-msh.el
@@ -10,8 +10,8 @@
   (add-to-list 'term-bind-key-alist '("C-c C-j" . term-line-mode))
   (add-to-list 'term-bind-key-alist '("C-c C-k" . term-char-mode))
 
-  ;; (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
-  ;; (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
+  (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+  (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
   (add-to-list 'term-bind-key-alist '("M-\\" . multi-term))
   )
 

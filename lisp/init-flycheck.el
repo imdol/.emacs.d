c6bfd6b15fd72d806884f5ff141abba1856cf233
imdol <imdol@protonmail.com>
2020-04-18 02:04:33 -0400

    change commonly used flycheck keys

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,7 +2,10 @@
 (setq-default
  flycheck-disabled-checkers '(emacs-lisp-checkdoc)
 ;; the default value was '(save idle-change new-line mode-enabled)
-;; having this enabled only checks syntax on SAVE
+ ;; having this enabled only checks syntax on SAVE
  flycheck-check-syntax-automatically '(save mode-enable))
+(global-set-key (kbd "C-c f l") #'flycheck-list-errors)
+(global-set-key (kbd "C-c f n") #'flycheck-next-error)
+(global-set-key (kbd "C-c f p") #'flycheck-previous-error)
 
 (provide 'init-flycheck)

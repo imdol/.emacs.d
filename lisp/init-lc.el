ddd33d8b604e2e8883696f34b2a90574cd6c5817
imdol <imdol@protonmail.com>
2020-04-26 06:49:15 -0400

    add sql preference for lc problems

--- lisp/init-lc.el
+++ lisp/init-lc.el
@@ -1,3 +1,6 @@
 (setq leetcode-prefer-language "cpp")
+(setq leetcode-prefer-sql "postgres")
+
+;; leetcode doesnt allow 3rd party logins. restore sessino from local chrome cookies with pip3 install my_cookies
 
 (provide 'init-lc)

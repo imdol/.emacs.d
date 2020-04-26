91d705e6f104e5f2fe0559cd866f4e26c248b0eb
imdol <imdol@protonmail.com>
2020-04-26 06:49:47 -0400

    remove melpa stable

--- init.el
+++ init.el
@@ -12,7 +12,6 @@
 (let ((file-name-handler-alist nil)) 
   ;; MELPA package
   (require 'package)
-  ;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize)
   

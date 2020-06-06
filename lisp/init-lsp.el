ab4368c1f767841652621e3ee5fa16a67e0290a0
imdol <imdol@protonmail.com>
2020-06-06 03:49:43 -0400

    push to use CCLS and strip some lsp setting defaults

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,23 +1,24 @@
-;; LSP config here for future use
-;; (require 'lsp-mode)
-;; (require 'company-lsp)
-;; (push 'company-lsp company-backends)
+;; LSP settings
+(with-eval-after-load 'lsp-mode
+  (setq
+   lsp-enable-file-watchers nil
+   lsp-enable-snippet nil
+   lsp-enable-completion-at-point nil
+   lsp-enable-symbol-highlighting nil
+   
+   lsp-print-performance nil
+   lsp-auto-guess-root nil
+   lsp-eldoc-enable-hover t
+   lsp-eldoc-render-all t
+   lsp-auto-configure t
+   lsp-prefer-flymake nil
+   lsp-response-timeout 4
+   )
+  ;; company LSP settings
+  (setq
+   company-lsp-enable-snippet t
+   ))
 
-;; lsp settings
-(setq
- lsp-enable-snippet t
- lsp-print-performance nil
- lsp-auto-guess-root nil
- lsp-eldoc-enable-hover t
- lsp-eldoc-render-all t
- lsp-auto-configure t
- lsp-prefer-flymake nil
- lsp-response-timeout 4
- )
 
-;; company lsp settings
-(setq
- company-lsp-enable-snippet t
- )
 
 (provide 'init-lsp)

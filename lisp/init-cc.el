52df4804cb2d7c451cfa4e274001e99ed11507d7
imdol <imdol@protonmail.com>
2020-06-22 01:56:57 -0400

    make .h files use c++ modes

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -1,13 +1,12 @@
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 (defun cc-lsp-setup ()
   (setq c-basic-offset 2
    indent-tabs-mode nil
    tab-width 2
    sp-escape-quotes-after-insert nil)
-  
   (yas-minor-mode +1)  
   (smartparens-mode +1)
   )
-
 (add-hook 'c++-mode-hook 'cc-lsp-setup)
 (add-hook 'c++-mode-hook 'lsp-deferred)
 

9f8c9976f1dd4964707f595c2f8bafa6d513bf5e
imdol <imdol@protonmail.com>
2020-07-02 16:00:13 -0400

    remove iy go to char conitnue back

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -1,6 +1,4 @@
 ;; avy and iy
 (global-set-key (kbd "C-;") 'iy-go-up-to-char)
-(global-set-key (kbd "'") 'iy-go-to-char-continue-backward)
 (global-set-key (kbd "C-'") 'avy-goto-word-1)
-
 (provide 'init-avy)

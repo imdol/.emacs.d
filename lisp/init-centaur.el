c9bb5046365a7100a75aef9f32d462e135743e9e
imdol <imdol@protonmail.com>
2020-08-30 04:49:00 -0400

    different centaur tab configs

--- lisp/init-centaur.el
+++ lisp/init-centaur.el
@@ -1,3 +1,5 @@
+(centaur-tabs-mode t)
+
 ;; basic settings
 (setq-default
  centaur-tabs-height 25
@@ -9,7 +11,6 @@
  centaur-tabs-cycle-scope 'tabs)
 
 ;; enable and inherit themes
-(centaur-tabs-mode t)
 (centaur-tabs-headline-match)
 (centaur-tabs-change-fonts "hack" 100)
 
@@ -26,21 +27,24 @@
 ;; def buffer groups
 (defun centaur-tabs-buffer-groups ()
   "`centaur-tabs-buffer-groups' control buffers' group rules.
-    Group centaur-tabs with mode if buffer is derived from
- `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
+    Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
     All buffer name start with * will group to \"Emacs\".
     Other buffer group by `centaur-tabs-get-group-name' with project name."
-  (list 
+  (list
    (cond
-    (string-equal "*" (substring (buffer-name) 0 1)
-     "Emacs")
+    ((string-equal "*vterm" (substring (buffer-name) 0 6))
+     "vterm")
+    ((string-equal "*docker" (substring (buffer-name) 0 7))
+     "docker")
+    ((string-equal "*" (substring (buffer-name) 0 1))
+     "emacs")
     ((derived-mode-p 'prog-mode)
-     "Editing")
+     "editing")
     ((derived-mode-p 'dired-mode)
-     "Dired")
+     "dired")
     ((memq major-mode '(helpful-mode
 			help-mode))
-     "Help")
+     "help")
     ((memq major-mode '(org-mode
 			org-agenda-clockreport-mode
 			org-src-mode
@@ -51,44 +55,35 @@
 			org-cdlatex-mode
 			org-agenda-log-mode
 			diary-mode))
-     "OrgMode")
-    ((memq major-mode '(magit-process-mode
-			magit-status-mode
-			magit-diff-mode
-			magit-log-mode
-			magit-file-mode
-			magit-blob-mode
-			magit-blame-mode
-			))
-     "Magit")
-    ((memq major-mode '(term-mode
-			eshell-mode
-			ansi-term-mode
-			))
-     "Shell")
+     "org")
     (t
      (centaur-tabs-get-group-name (current-buffer))))))
 
-;; hide tabs for certain buffers
+;;hide tabs for certain buffers
 (defun centaur-tabs-hide-tab (x)
   (let ((name (format "%s" x)))
     (or
      (string-prefix-p "*epc" name)
      (string-prefix-p "*helm" name)
      (string-prefix-p "*Compile-Log*" name)
+     (string-prefix-p "*Process List*" name)
      (string-prefix-p "*lsp" name)
      (string-prefix-p "*NeoTree*" name)
      (string-prefix-p "COMMIT_EDITMSG" name)
      (string-prefix-p "*tide-server*" name)
      (string-prefix-p "*GraphQL*" name)
      (string-prefix-p "*spotify" name)
+     (string-prefix-p "*Messages*" name)
+     (string-prefix-p "*scratch*" name)
      (and (string-prefix-p "magit" name)
 	  (not (file-name-extension name)))
      )))
 
 ;; group by projectile projects
+(centaur-tabs-group-buffer-groups)
 (centaur-tabs-group-by-projectile-project)
 
-(setq centaur-tabs-background-color "#ff0000")
+(global-set-key (kbd "C-c t p") 'centaur-tabs-group-by-projectile-project)
+(global-set-key (kbd "C-c t o") 'centaur-tabs-group-buffer-groups)
 
 (provide 'init-centaur)

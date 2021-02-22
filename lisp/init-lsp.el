d07912dd31e220841d41162b60f3dbfc61fbe9c0
imdol <imdol@protonmail.com>
2021-02-22 01:49:06 -0500

    formatting changes, remove LSP find def key with xref conflict

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,33 +1,49 @@
 ;; LSP settings
-(with-eval-after-load 'lsp-mode
-  (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
-  (setq
-   ;; basics
-   lsp-auto-configure t
-   lsp-enable-file-watchers nil
-   lsp-enable-symbol-highlighting nil
-   lsp-auto-configure t
-   lsp-print-performance nil
-   lsp-auto-guess-root nil
-   lsp-response-timeout 4
-   lsp-prefer-flymake nil
-   
-   ;; help
-   lsp-eldoc-render-all nil
-   lsp-eldoc-enable-hover t
-   
-   ;; completion
-   lsp-prefer-capf t
-   lsp-idle-delay 0.2
-   lsp-enable-snippet t
-   lsp-enable-completion-at-point t
-   
-   ;; jumping
-   lsp-enable-xref t
-
-   ;; performance
-   lsp-print-performance t)
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+;;  :bind (("M-." . lsp-find-definition))
+  :config
+  (lsp-enable-which-key-integration t)
+  :custom
+  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-enable-file-watchers nil)
+  (lsp-auto-guess-root nil)
+  (lsp-response-timeout 4)
+  (lsp-prefer-flymake nil)
+  (lsp-enable-xref t)
   )
 
-(setq eldoc-idle-delay 0)
+;; (with-eval-after-load 'lsp-mode
+;;   (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
+;;   (setq
+;;    ;; basics
+;;    lsp-auto-configure t
+;;    lsp-enable-file-watchers nil
+;;    lsp-enable-symbol-highlighting nil
+;;    lsp-auto-configure t
+;;    lsp-print-performance nil
+;;    lsp-auto-guess-root nil
+;;    lsp-response-timeout 4
+;;    lsp-prefer-flymake nil
+   
+;;    ;; help
+;;    lsp-eldoc-render-all nil
+;;    lsp-eldoc-enable-hover t
+   
+;;    ;; completion
+;;    lsp-prefer-capf t
+;;    lsp-idle-delay 0.2
+;;    lsp-enable-snippet t
+;;    lsp-enable-completion-at-point t
+   
+;;    ;; jumping
+;;    lsp-enable-xref t
+
+;;    ;; performance
+;;    lsp-print-performance t)
+;;   )
+
+;; (setq eldoc-idle-delay 0)
 (provide 'init-lsp)
--- lisp/init-prog-env.el
+++ lisp/init-prog-env.el
@@ -5,8 +5,7 @@
 (setq
  which-key-show-early-on-C-h t
  which-key-idle-delay 10000
- which-key-idle-secondary-delay 0.05
-      )
+ which-key-idle-secondary-delay 0.05)
 (which-key-mode)
 
 ;; set backspace char deletes to hungry

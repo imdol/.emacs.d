f38dceec8bfadae0040a37f83e55f0c00fc18faf
imdol <imdol@protonmail.com>
2021-02-22 01:47:23 -0500

    iy deprecated in MELPA, use jump char forward

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -1,4 +1,5 @@
 ;; avy and iy
-(global-set-key (kbd "C-;") 'iy-go-up-to-char)
+;;(global-set-key (kbd "C-;") 'iy-go-up-to-char)
+(global-set-key (kbd "C-;") 'jump-char-forward)
 (global-set-key (kbd "C-'") 'avy-goto-word-1)
 (provide 'init-avy)

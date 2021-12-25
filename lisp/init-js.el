f4efb0bf2c171d00556a9a18bad707c0fc01c269
imdol <imdol@protonmail.com>
2021-12-25 00:55:34 -0500

    add dap

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -53,6 +53,10 @@
   (yas-global-mode +1)
 
   (hungry-delete-mode +1)
+
+  ;; dap-mode
+  (dap-mode 1)
+  (require 'dap-node)
   
   ;; fence edit for graphql mode in rjsx
   ;; eval only for js related mdoes
@@ -78,10 +82,35 @@
   (global-set-key (kbd "C-c '") 'fence-edit-dwim)
   )
 
+(defun js-lsp-setup ()
+  (setq-default
+   js2-basic-offset 2
+   js-indent-align-list-continuation nil)
+  (setq
+   tab-width 2
+   indent-tabs-mode nil)
+  
+  (emmet-mode +1)
+  (setq emmet-expand-jsx-className? t)
+  (yas-global-mode +1)
+  (hungry-delete-mode +1)
+  (flycheck-mode +1)
+  (smartparens-mode +1)
+
+  ;; dap-mode
+  (dap-mode 1)
+  (require 'dap-chrome)
+  (require 'dap-node)
+
+  (define-key js-mode-map (kbd "M-.") nil)
+  )
+
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
 ;; set up tide mode after js2 mode 
-(add-hook 'rjsx-mode-hook #'setup-tide-mode)
+;;(add-hook 'rjsx-mode-hook #'setup-tide-mode)
+(add-hook 'rjsx-mode-hook 'js-lsp-setup)
+(add-hook 'rjsx-mode-hook 'lsp-deferred)
 
 (provide 'init-js)

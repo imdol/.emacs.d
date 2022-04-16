e3a254db294a692a92c4f18a2610b600d5d5ced3
imdol <imdol@protonmail.com>
2022-04-16 13:03:13 -0400

    go to use tabs, hungry deletes

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -3,13 +3,16 @@
 
 (defun go-lsp-setup ()
   (setq c-basic-offset 4
-   indent-tabs-mode nil
+   indent-tabs-mode 1
    tab-width 4
    sp-escape-quotes-after-insert nil)
 
   (company-mode 1)
   (yas-minor-mode +1)  
   (smartparens-mode +1)
+  (hungry-delete-mode +1)
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t)  
   )
 
 (add-hook 'go-mode-hook 'go-lsp-setup)

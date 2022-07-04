68aeee79560385b7de2dac1afee90310e1d896f7
imdol <imdol@protonmail.com>
2022-07-04 19:40:16 -0400

    kill tide, for lsp

--- lisp/init-ts.el
+++ lisp/init-ts.el
@@ -1,31 +1,24 @@
 ;; hook relevant modes
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
 
-;; define tide mode settings
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-
+(defun ts-lsp-setup ()
   (setq-default typescript-indent-level 2)
-
-  ;; use tsfmt.json instead and add to .gitignore or do it manually here
-  (setq tide-format-options '(:indentSize 2 :tabSize 2 :placeOpenBraceOnNewLineForFunctions nil))
-  (flycheck-mode +1)
-  (smartparens-mode +1)
-  (company-mode +1)
+  (setq indent-tabs-mode nil)
+  
   (yas-global-mode +1)
   (hungry-delete-mode +1)
+  (flycheck-mode +1)
+
+  ;; dap-mode
+  (dap-mode 1)
+  (require 'dap-node)
   )
 
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
 ;; formats the buffer before saving
-(add-hook 'before-save-hook 'tide-format-before-save)
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
+(add-hook 'typescript-mode-hook 'ts-lsp-setup)
+(add-hook 'typescript-mode-hook 'lsp-deferred)
 
 (provide 'init-ts)

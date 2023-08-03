21d6d08ee3ee22206f82feef1c289af5743cc598
imdol <imdol@protonmail.com>
2023-08-03 15:25:23 -0400

    helm buffer heights changed to be smaller

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -13,6 +13,12 @@
  	       (display-buffer-in-side-window)
  	       (inhibit-same-window . t)
  	       (window-height . 0.3)))
+  ;; adjusts helm header
+  (defun helm-toggle-source-header-line ()
+    (if (= (length (ignore-errors (with-helm-buffer helm-sources))) 1)
+	(set-face-attribute 'helm-source-header nil :height 1.0)
+      (set-face-attribute 'helm-source-header nil :height 1.0)))
+  (add-hook 'helm-after-initialize-hook 'helm-toggle-source-header-line)
   )
 
 (provide 'init-helm)
--- lisp/init-py.el
+++ lisp/init-py.el
@@ -1,3 +1,5 @@
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-ts-mode))
+
 (defun pyls-mode-setup()
   (setq
    indent-tabs-mode nil
@@ -5,7 +7,6 @@
    tab-width 4)
   
   (company-mode +1)
-  
   (smartparens-mode +1)
   (yas-global-mode +1)
 
@@ -19,9 +20,14 @@
   	  company-oddmuse))
 
   (setq lsp-pyls-plugins-jefi-hover-enabled nil)
+
+  (define-key python-ts-mode-map (kbd "M-e") 'forward-sentence)
+  (define-key python-ts-mode-map (kbd "M-a") 'backward-sentence)
   )
 
-(add-hook 'python-mode-hook 'lsp-deferred)
-(add-hook 'python-mode-hook #'pyls-mode-setup)
+(add-hook 'python-ts-mode-hook 'lsp-deferred)
+(add-hook 'python-ts-mode-hook #'pyls-mode-setup)
+
+
 
 (provide 'init-py)

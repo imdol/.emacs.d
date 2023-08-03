b159b0f1017bd232a80ee3519fea12a05eecac87
imdol <imdol@protonmail.com>
2023-08-03 19:39:14 -0400

    remove custom set faces on company popups

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -11,14 +11,6 @@
     (define-key company-active-map (kbd "C-d") #'company-abort)
     (define-key company-active-map (kbd "C-n") #'company-select-next)
     (define-key company-active-map (kbd "C-p") #'company-select-previous))
-  (require 'color)
-  (let ((bg (face-attribute 'default :background)))
-    (custom-set-faces
-     `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 10)))))
-     `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-     `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-     `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-     `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
   )
 
 (provide 'init-company)

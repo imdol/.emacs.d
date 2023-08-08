9d3cbe01ab75697553012abf6ce619decfeadbb4
imdol <imdol@protonmail.com>
2023-08-08 17:41:07 -0400

    rust modes to use treesitter

--- lisp/init-rs.el
+++ lisp/init-rs.el
@@ -1,6 +1,8 @@
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
+
 (defun rs-lsp-setup ()
   (yas-minor-mode +1)
   (smartparens-mode +1)
   )
-(add-hook 'rust-mode-hook 'rs-lsp-setup)
-(add-hook 'rust-mode-hook 'lsp-deferred)
+(add-hook 'rust-ts-mode-hook 'rs-lsp-setup)
+(add-hook 'rust-ts-mode-hook 'lsp-deferred)

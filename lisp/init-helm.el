e1793e527229bfaf338d319c7e0f2d25347afb8b
imdol <imdol@protonmail.com>
2023-08-07 23:49:08 -0400

    extra key for exiting mini buffer

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -4,15 +4,17 @@
   :bind (("M-x" . helm-M-x)
 	 ("C-x C-f" . helm-find-files)
 	 ("C-x C-b" . helm-buffers-list)
-	 ("C-x b" . helm-mini)	 
-	 ("M-y" . helm-show-kill-ring))
+	 ("C-x b" . helm-mini)
+	 ("M-y" . helm-show-kill-ring)
+	 :map helm-map
+	 ([tab] . helm-maybe-exit-minibuffer))
   :config
   (helm-mode 1)
   (add-to-list 'display-buffer-alist
- 	     `(,(rx bos "*helm" (* not-newline) "*" eos)
- 	       (display-buffer-in-side-window)
- 	       (inhibit-same-window . t)
- 	       (window-height . 0.3)))
+ 	       `(,(rx bos "*helm" (* not-newline) "*" eos)
+ 		 (display-buffer-in-side-window)
+ 		 (inhibit-same-window . t)
+ 		 (window-height . 0.3)))
   ;; adjusts helm header
   (defun helm-toggle-source-header-line ()
     (if (= (length (ignore-errors (with-helm-buffer helm-sources))) 1)

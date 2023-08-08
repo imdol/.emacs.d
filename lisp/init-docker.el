89710eedcd44eeda5f391b0154ee03b228f47003
imdol <imdol@protonmail.com>
2023-08-08 17:40:45 -0400

    treesitter integration with docker

--- lisp/init-docker.el
+++ lisp/init-docker.el
@@ -1,8 +1,10 @@
+(add-to-list 'auto-mode-alist '("Dockerfile" . dockerfile-ts-mode))
+
 (defun docker-setup ()
   (yas-minor-mode +1)
   (smartparens-mode +1)
   )
-(add-hook 'docker-mode-hook 'docker-setup)
+(add-hook 'dockerfile-ts-mode-hook 'docker-setup)
 (global-set-key (kbd "C-c d") 'docker)
 
 (provide 'init-docker)

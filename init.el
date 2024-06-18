6b3bb5dff32490546b4b87569931e98944991e57
imdol <imdol@protonmail.com>
2024-06-18 16:40:28 -0400

    refactored init config, split files

--- init.el
+++ init.el
@@ -10,47 +10,67 @@
 ;; 	    (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 ;; benchmarking startup
-;; (require 'benchmark-init)
-;; (add-hook 'after-init-hook 'benchmark-init/deactivate)
+(require 'benchmark-init)
+(add-hook 'after-init-hook 'benchmark-init/deactivate)
 
 (let ((file-name-handler-alist nil))
   (setq custom-file (concat user-emacs-directory "custom.el"))
   (when (file-exists-p custom-file)
     (load custom-file))
-  
+
   ;; MELPA package
   (require 'package)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize)
-  
+
   ;; extend load path for .el
   (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-  
+
   ;; install packages if they're missing
   (unless package-archive-contents
     (package-refresh-contents)
     (package-install-selected-packages))
-  
+
   ;; sys settings
   (require 'init-sys-env)
   (require 'init-window-frames)
   (require 'init-vterm)
+
+  ;; macros
   (require 'init-hydra)
-  
-  ;; prog settings + vc
+
+  ;; prog settings
   (require 'init-prog-env)
-  (require 'init-smartparens)
   (require 'init-treesitter)
+
+  ;; UI
+  (require 'init-orderless) ;; regex completion for completing-read and child buffers
+  (require 'init-vertico) ;; completing-read minibuffer list commands
+  (require 'init-marginalia) ;; completing-read minibuffer annotations
+  (require 'init-consult) ;; completing-read minibuffer enhancements
+  (require 'init-embark) ;;
+  (require 'init-corfu) ;; child buffer for CAPFs
+  ;; (require 'init-company)
+
+  ;; parens
+  (require 'init-smartparens)
+
+  ;; file tree
   (require 'init-treemacs)
+
+  ;; projects
   (require 'init-project)
-  (require 'init-flycheck)
+
+  ;; VC
   (require 'init-magit)
-  
-  ;; nav + completion
-  (require 'init-vemco)
-  (require 'init-company)
+
+  ;; syntax + linting
+  (require 'init-flymake)
+  ;; (require 'init-flycheck)  
+
+  ;; CAPF providers
   (require 'init-lsp)
-  
+
   ;; lang modes
   (require 'init-c)
   (require 'init-cc)

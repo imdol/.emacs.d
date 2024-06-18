2d6f9d14231b36274993dbba1554c78bc73adf8e
imdol <imdol@protonmail.com>
2024-06-18 16:49:32 -0400

    changes to lsp config

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,53 +1,39 @@
 ;; LSP settings
 (use-package lsp-mode
+  :defer t
   :commands (lsp lsp-deferred)
   :init
   (setq lsp-keymap-prefix "C-c l")
+
   :config
   (lsp-enable-which-key-integration t)
-  :custom
-  (lsp-headerline-breadcrumb-enable nil)
-  (lsp-enable-file-watchers nil)
-  (lsp-auto-guess-root nil)
-  (lsp-response-timeout 4)
-  (lsp-prefer-flymake nil)
-  (lsp-enable-xref t)
-  (lsp-lens-enable nil)
+  (setq
+   ;; basics
+   lsp-auto-configure t
+   lsp-enable-file-watchers nil
+   lsp-enable-symbol-highlighting t
+   lsp-enable-folding t
+   lsp-enable-symbol-highlighting t
+   lsp-auto-guess-root nil
+
+   ;; UI
+   lsp-headerline-breadcrumb-enable nil
+   
+   ;; jumping 
+   lsp-enable-xref t
+
+   ;; linting
+   lsp-prefer-flymake t
+   lsp-response-timeout 5
+   lsp-lens-enable nil
+
+   ;;completion
+   lsp-enable-snippet t
+   lsp-idle-delay 0.3
+   lsp-completion-enable t
+   lsp-completion-show-detail t
+   
+   )
+
   )
-
-(use-package helm-lsp
-  :after (lsp)
-  )
-
-;; (with-eval-after-load 'lsp-mode
-;;   (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
-;;   (setq
-;;    ;; basics
-;;    lsp-auto-configure t
-;;    lsp-enable-file-watchers nil
-;;    lsp-enable-symbol-highlighting nil
-;;    lsp-auto-configure t
-;;    lsp-print-performance nil
-;;    lsp-auto-guess-root nil
-;;    lsp-response-timeout 4
-;;    lsp-prefer-flymake nil
-   
-;;    ;; help
-;;    lsp-eldoc-render-all nil
-;;    lsp-eldoc-enable-hover t
-   
-;;    ;; completion
-;;    lsp-prefer-capf t
-;;    lsp-idle-delay 0.2
-;;    lsp-enable-snippet t
-;;    lsp-enable-completion-at-point t
-   
-;;    ;; jumping
-;;    lsp-enable-xref t
-
-;;    ;; performance
-;;    lsp-print-performance t)
-;;   )
-
-;; (setq eldoc-idle-delay 0)
 (provide 'init-lsp)

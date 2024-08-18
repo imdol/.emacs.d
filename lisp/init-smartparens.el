a03ea13c7befbaf5289e375696218e48aaf33e10
imdol <imdol@protonmail.com>
2024-08-17 21:42:57 -0400

    enable smart parens global

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -1,7 +1,9 @@
 (use-package smartparens
   :defer t
   :hook
-  (prog-mode-hook . smartparens-mode)
+  (prog-mode-hook . smartparens-global-mode)
+  :init
+  (smartparens-global-mode)
   :config
   (sp-pair "`" nil :post-handlers '(:add ("||\n[i]" "RET")))
   (sp-pair "(" nil :post-handlers '(:add ("||\n[i]" "RET")))

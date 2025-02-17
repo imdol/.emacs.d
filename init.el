da8d79298e3f2957a9f2aaa22a9a8b58aff2f8b1
imdol <imdol@protonmail.com>
2025-02-17 00:20:50 -0500

    add mood-line

--- init.el
+++ init.el
@@ -60,6 +60,15 @@
   (after-init . which-key-mode)
   )
 
+(use-package mood-line
+  :ensure t
+  :config
+  (mood-line-mode)
+  ;; Use pretty Fira Code-compatible glyphs
+  :custom
+  (mood-line-glyph-alist mood-line-glyphs-fira-code)
+  )
+
 (use-package drag-stuff
   :ensure t
   :defer t
@@ -526,12 +535,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(modus-vivendi))
- '(package-selected-packages
-   '(ace-window beacon company-quickhelp corfu drag-stuff eglot
-		embark-consult emmet-mode expand-region hydra iedit
-		lsp-mode magit marginalia multiple-cursors orderless
-		pyvenv rainbow-delimiters smartparens typescript-mode
-		vertico yasnippet-snippets)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

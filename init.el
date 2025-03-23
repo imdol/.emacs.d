dac17914e7f11b6d026faebd618f7cfc9fdda7a4
imdol <imdol@protonmail.com>
2025-03-23 09:21:11 -0400

    add benchmark init

--- init.el
+++ init.el
@@ -11,6 +11,12 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
+(use-package benchmark-init
+  :ensure t
+  :config
+  ;; To disable collection of benchmark data after init is done.
+  (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
 (use-package emacs
   :hook
   (prog-mode-hook . hs-minor-mode)
@@ -571,15 +577,16 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(modus-vivendi))
  '(package-selected-packages
-   '(ace-window beacon corfu doom-modeline drag-stuff eglot
-		embark-consult emmet-mode expand-region hydra iedit
-		lsp-mode magit marginalia mood-line multiple-cursors
-		orderless poetry pos-tip rainbow-delimiters restclient
-		smartparens typescript-mode vertico yasnippet-snippets)))
+   '(ace-window beacon benchmark-init corfu doom-modeline drag-stuff
+		eglot embark-consult emmet-mode expand-region hydra
+		iedit lsp-mode magit marginalia mood-line
+		multiple-cursors orderless poetry pos-tip
+		rainbow-delimiters restclient smartparens
+		typescript-mode vertico yasnippet-snippets))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
